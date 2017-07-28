7af0fa369dc82595f808b335658931e32121986a
KhorinVitaly <khorin.v@gmail.com>
2017-07-28 14:19:27 +0600

    first commit, solution by task

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,44 @@
+import requests
+from datetime import datetime
+
+
+def get_seven_days_ago_date():
+    current_date = datetime.today()
+    current_timestamp = current_date.timestamp()
+    return datetime.fromtimestamp(current_timestamp - (86400 * 7)).date()
 
 
 def get_trending_repositories(top_size):
-    pass
+    isoformat_date = get_seven_days_ago_date.isoformat()
+    search_parameters = {'q': 'created:>{0}'.format(isoformat_date),
+                         'sort': 'stars',
+                         'order': 'desc'}
+    response = requests.get('https://api.github.com/search/repositories', search_parameters)
+    result = response.json()
+    return result['items'][:top_size]
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_info(repo_owner, repo_name):
+    url = 'https://api.github.com/repos/{0}/{1}/issues'.format(repo_owner, repo_name)
+    response = requests.get(url, {'state': 'open'})
+    result = response.json()
+    if response.status_code == 200:
+        return ', Open issues amount: {0}'.format(len(result))
+    else:
+        return ', Issues count failed: {0}'.format(result['message'])
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 20
+    try:
+        repositories = get_trending_repositories(top_size)
+        for repo in repositories:
+            repo_url = repo['html_url']
+            open_issues_info = get_open_issues_info(repo['owner']['login'], repo['name'])
+            print('{0}{1}'.format(repo_url, open_issues_info))
+    except requests.HTTPError as error:
+        print('HTTP Error')
+        print('Response is: {0}'.format(error.response.content))
+    except requests.ConnectionError:
+        print('Connection failed')
+

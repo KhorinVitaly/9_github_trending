c6db73854e4ba44e518121ec7d7edfc03b67df17
KhorinVitaly <khorin.v@gmail.com>
2017-07-30 23:05:48 +0600

    fix mistakes, do code optimization
    Less code, one request, request optimizely

--- github_trending.py
+++ github_trending.py
@@ -2,39 +2,33 @@ import requests
 from datetime import datetime, timedelta
 
 
-def get_seven_days_ago_date():
+def get_past_date(count_of_day_ago):
     current_date = datetime.today().date()
-    return current_date - timedelta(days=7)
+    return current_date - timedelta(days=count_of_day_ago)
 
 
 def get_trending_repositories(top_size):
-    isoformat_date = get_seven_days_ago_date().isoformat()
-    search_parameters = {'q': 'created:>{0}'.format(isoformat_date),
+    count_of_day_ago = 7
+    iso_format_date = get_past_date(count_of_day_ago).isoformat()
+    search_parameters = {'q': 'created:>{0}'.format(iso_format_date),
                          'sort': 'stars',
-                         'order': 'desc'}
+                         'order': 'desc',
+                         'page': '1',
+                         'per_page': str(top_size)}
     response = requests.get('https://api.github.com/search/repositories', search_parameters)
     result_as_json = response.json()
-    return result_as_json['items'][:top_size]
-
-
-def get_open_issues_info(repo_owner, repo_name):
-    url = 'https://api.github.com/repos/{0}/{1}/issues'.format(repo_owner, repo_name)
-    response = requests.get(url, {'state': 'open'})
-    result_as_json = response.json()
-    if response.status_code == 200:
-        return ', Open issues amount: {0}'.format(len(result_as_json))
-    else:
-        return ', Issues count failed: {0}'.format(result_as_json['message'])
+    return result_as_json['items']
 
 
 if __name__ == '__main__':
     top_size = 20
     try:
         repositories = get_trending_repositories(top_size)
+        print(len(repositories))
         for repo in repositories:
             repo_url = repo['html_url']
-            open_issues_info = get_open_issues_info(repo['owner']['login'], repo['name'])
-            print('{0}{1}'.format(repo_url, open_issues_info))
+            open_issues_count = repo['open_issues_count']
+            print('{0}, open issues count: {1}'.format(repo_url, open_issues_count))
     except requests.HTTPError as error:
         print('HTTP Error')
         print('Response is: {0}'.format(error.response.content))
